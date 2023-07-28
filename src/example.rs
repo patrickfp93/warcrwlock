63ec7ffaf78902964ffb63f345bd4f3191dfd93d
Patrick Fernandes Peçanha <patrickfp93@gmail.com>
2023-07-28 17:43:14 -0300

    update exemple.rs

--- src/example.rs
+++ src/example.rs
@@ -1,84 +1,199 @@
  /*
- # WarcRwLock
+# WarcRwLock
 
- The `warcrwlock` crate provides a convenient attribute `#[warcrwlock]` that can be used on mods, structs, and impls in Rust code.
+[![Crates.io](https://img.shields.io/crates/v/warcrwlock.svg)](https://crates.io/crates/warcrwlock)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
- ## Description
+## Description
 
- The purpose of this crate is to generate a wrapper around structs that allows them to be used with asynchronous reference control (`Arc`) and the `RwLock` for asynchronous mutation control. The generated code handles the synchronization and locking mechanisms, making it easier to work with concurrent data in multi-threaded applications.
+The WarcRwLock crate is a Rust library that provides a macro attribute for mods, structs, and impls. The purpose of this library is to generate a wrapper that allows the struct to be used with the asynchronous reference control called Arc and the RwLock for asynchronous mutation control.
 
- ## Usage
+## Installation
 
- To use the `#[warcrwlock]` attribute, simply add it to a mod, struct, or impl definition. The code inside the mod, struct, or impl will be automatically transformed to use `Arc` and `RwLock` for thread-safe access.
+To use the WarcRwLock crate, add the following dependency to your `Cargo.toml`:
 
- ## Attribute
+```toml
+[dependencies]
+warcrwlock = "1.2.0"
+```
 
- ```rust
- #[warcrwlock]
- ```
+## Example Usage
 
- The `#[warcrwlock]` attribute is used to mark mods, structs, or impls that need to be transformed for concurrent access.
+Here's a simple example of using WarcRwLock:
 
- ## Example
+```rust
+#[warcrwlock]
+pub struct MyStruct {
+    value: usize,
+}
 
- ```rust
- // This example will be included from the file "example.rs"
- #[doc(include = "./example.rs")]
- ```
+#[warcrwlock]
+impl MyStruct {
+    pub fn new(value : usize) -> Self {
+        Self {
+            value,
+        }
+    }
 
- After applying the `#[warcrwlock]` attribute, the code is transformed into:
+    pub fn reset(&mut self) {
+        self.value = 0;
+    }
 
- ```rust
- use std::sync::{Arc, RwLock};
+    pub fn value_mut(&mut self) -> &mut usize {
+        &mut self.value
+    }
 
- pub struct MyStructBase {
-     value: usize,
- }
+    pub fn get_value(&self) -> usize {
+        self.value
+    }
+    
+    //wrapper Method
+    pub fn plus(a : MyStruct, b : MyStruct) -> MyStruct{
+        *a.write().unwrap().value_mut() += b.get_value();
+        a
+    }
+    #[wrapper_method]
+    pub fn get_wrapper_value(&self)-> usize{
+        self.get_value()
+    }
 
- impl MyStructBase {
-     pub fn new() -> Self {
-         Self {
-             value: 0,
-         }
-     }
+    pub fn child(&self) -> MyStruct{
+        MyStruct::new(self.value + 10)
+    }
+    
+}
+```
 
-     pub fn increment(&mut self) {
-         self.value += 1;
-     }
+After applying the `#[warcrwlock]` attribute, the code is transformed into:
 
-     pub fn get_value(&self) -> usize {
-         self.value
-     }
- }
+```rust
+pub struct MyStructBase {
+    value: usize,
+}
 
- pub struct MyStruct {
-     base: Arc<RwLock<MyStructBase>>,
- }
+impl MyStructBase {
+    pub fn new(value : usize) -> Self {
+        Self {
+            value,
+        }
+    }
 
- impl MyStruct {
-     pub fn new() -> Self {
-         Self {
-             base: Arc::new(RwLock::new(MyStructBase::new())),
-         }
-     }
+    fn reset(&mut self) {
+        self.value = 0;
+    }
 
-     pub fn increment(&mut self) {
-         self.base.write().unwrap().increment();
-     }
+    pub fn value_mut(&mut self) -> &mut usize {****
+        &mut self.value
+    }
 
-     pub fn get_value(&self) -> usize {
-         self.base.read().unwrap().get_value()
-     }
- }
- ```
+    fn get_value(&self) -> usize {
+        self.value
+    }
+    
+    pub fn child(&self) -> MyStructBase{
+        MyStructBase::new(self.value + 10)
+    }
 
- The `MyStruct` struct is automatically rewritten with the addition of a `base` field containing an `Arc<RwLock<MyStructBase>>`, and the methods are implemented to safely access the `base` field.
+}
 
- ## Contribution
+pub struct MyStruct {
+    base: Arc<RwLock<MyStructBase>>,
+}
 
- The WarcRwLock project is mainly maintained by a single developer known as PFP but welcomes contributions from the community. However, it's essential that contributions stay within the scope of the project's main function.
+impl MyStruct {
+    pub fn new(value : usize) -> Self {
+        Self {
+            base: Arc::new(RwLock::new(MyStructBase::new(value))),
+        }
+    }
 
- ## License
+    pub fn reset(&mut self) {
+        self.base.write().unwrap().reset();
+    }
 
- This project is licensed under the MIT License. See the [LICENSE](../LICENSE) file for more details.
+    pub fn get_value(&self) -> usize {
+        self.base.read().unwrap().get_value()
+    }
+    
+    pub fn plus(a : MyStruct, b : MyStruct) -> MyStruct{
+        *a.write().unwrap().value_mut() = b.get_value();
+        a
+    }
+
+    pub fn get_wrapper_value(&self)-> usize{
+        self.get_value()
+    }
+
+    pub fn child(&self) -> MyStruct{
+        MyStruct {
+            base: Arc::new(RwLock::new(self.child())),
+        }
+    }    
+
+}
+
+impl MyStruct {
+    pub fn read(&self) -> RwLockReadGuard<'_, MyStructBase> {
+        self.base.read().unwrap()
+    }
+
+    pub fn write(&self) -> RwLockWriteGuard<'_, MyStructBase> {
+        self.base.write().unwrap()
+    }
+}
+
+impl Clone for MyStruct {
+    fn clone(&self) -> Self {
+        Self {
+            base: self.base.clone(),
+        }
+    }
+}
+
+unsafe impl Send for MyStruct {}
+unsafe impl Sync for MyStruct {}
+```
+
+After using the `#[warcrwlock]` attribute, the `MyStruct` will be automatically rewritten with the addition of a `base` field containing an `Arc<RwLock<MyStructBase>>`. The functions of `MyStruct` will then be implemented to safely access the `base` field.
+
+>`Update(1.1.0)`: Methods with parameters of the same type as the presented structure can now be freely implemented.
+>`Update(1.2.0)`: Added Wrapper Methods and allowed all types of methods with Self return.
+### Wrapper Methods
+This type of method happens for two reasons: when the method has parameters of type ``Self`` or when the ``wrapper_attribute`` attribute is added.
+
+### Method read and write
+Similar to the methods in `RwLock<T>`, these functions are used to lock the usage and gain access to read or write functions, as shown in the example below:
+```rust
+fn main() {
+    use my_module::MyStruct;
+    let mut a = MyStruct::new();
+    *a.write().unwrap().value_mut() = 10;
+    assert_eq!(a.read().unwrap().get_value(), 10);
+    let a = MyStruct::plus(a, MyStruct::new(50));    
+    assert_eq!(a.read().unwrap().get_value(), 60);    
+}
+```
+
+### Modules
+You can simplify the use of `#[warcrwlock]` by placing it as an attribute for the module, which will have the same effect as in the previous example:
+```rust
+use warcrwlock::warcrwlock;
+
+#[warcrwlock]
+mod my_module {
+    /// other mods, structs, and/or impls...
+}
+```
+
+> When used on a module, all structs, impls, and mods will be included, with exceptions.
+
+> The use of the attribute may not work well with other attributes.
+
+## Contribution
+
+The WarcRwLock project is mainly maintained by a single developer known as PFP but welcomes contributions from the community. However, it's essential that contributions stay within the scope of the project's main function.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
  */
